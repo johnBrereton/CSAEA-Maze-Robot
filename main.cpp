c9ad030ab941f753bcf228ad4ff9da5259b6be3b
John <john@jbrereton.com>
2021-11-17 18:59:48 -0500

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,13 +10,12 @@ const int m2b = 9;
 
 const int turnTime = 2000; // Time in ms required for robot to turn 90 degrees
 
-const int ledR = 11;
-const int ledG = 12;
-const int ledB = 13;
-
+const int redLED = 11;
+const int greenLED = 12;
 const int leftLED = 7;
 const int rightLED = 8;
 
+Servo servo;
 int servoPosition = 0;
 
 const int trig = 4;
@@ -34,10 +33,12 @@ void setup() {
 
 void loop() {
     if (enabled && digitalRead(2) == HIGH) {
+        digitalWrite(greenLED, LOW);
         enabled = false;
         stop();
     }
     else if (!enabled && digitalRead(2) == HIGH) {
+        digitalWrite(greenLED, HIGH);
         enabled = true;
     }
 
@@ -57,9 +58,7 @@ void pinSetup() {
     pinMode(m2a, OUTPUT);
     pinMode(m2b, OUTPUT);
 
-    // Set servo pin mode and created servo instance with servo library
-    pinMode(servo, OUTPUT);
-    Servo servo;
+    // Set servo pin to 11
     servo.attach(11);
 
     // Set ultrasonic sensor pin modes
@@ -86,22 +85,22 @@ void getDistances() {
 
 void driveUpdate() {
     // If the front and left sides are blocked turn right
-    if(right_dis > 8 && left_dis <= 8 && front_dis <= 8){
+    if(rightDistance > 8 && leftDistance <= 8 && frontDistance <= 8){
         turnRight();
     }
 
     // If the right and front sides are blocked turn left
-    else if (left_dis > 8 && right_dis <= 8 && front_dis <= 8){
+    else if (leftDistance > 8 && rightDistance <= 8 && frontDistance <= 8){
         turnLeft();
     }
 
     // If there is nothing in front and left and right are blocked proceed forward
-    else if (front_dis > 8){
+    else if (frontDistance > 8){
         forward();
     }
 
     // If all sides are blocked flash red LED
-    else if (front_dis <= 8){
+    else if (frontDistance <= 8){
         
     }
 }
@@ -122,6 +121,7 @@ float distance() {
     return calculatedDistance;
 }
 
+// Move robot forward indefinetly
 void forward() {
     digitalWrite(m1a, HIGH);
     digitalWrite(m1b, LOW);
@@ -129,14 +129,14 @@ void forward() {
     digitalWrite(m2b, LOW);
 }
 
-// Turn the robot left 90 degrees
+// Turn robot left 90 degrees
 void turnLeft() {
     digitalWrite(m1a, HIGH);
     digitalWrite(m1b, LOW);
     digitalWrite(m2a, LOW);
     digitalWrite(m2b, LOW);
 
-    blink(leftLED, turnTime);
+    blink(leftLED, turnTime, 4);
 
     digitalWrite(m1a, LOW);
 }
@@ -148,7 +148,7 @@ void turnRight() {
     digitalWrite(m2a, HIGH);
     digitalWrite(m2b, LOW);
 
-    blink(rightLED, turnTime);
+    blink(rightLED, turnTime, 4);
 
     digitalWrite(m2a, LOW);
 }
@@ -160,18 +160,19 @@ void stop() {
     digitalWrite(m2a, LOW);
     digitalWrite(m2b, LOW);
 
-    blink(ledR, int 10000);
+    blink(redLED, 10000, 10);
 }
 
-void blink(int led, int totalTime) {
+// Flash specified led 4 times over set period of time
+void blink(int led, int totalTime, int pulses) {
     for (int i = 0; i <= 3; i++) {
         digitalWrite(led, HIGH);
         
-        delay(totalTime/8);
+        delay(totalTime/(pulses*2));
         
         digitalWrite(led, LOW);
         
-        delay(totalTime/8);
+        delay(totalTime/(pulses*2));
     }
 }
 
